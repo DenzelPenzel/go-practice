84df4e7a2743b7c46dfc370876279c6bef975dee
denisschmidt <parasolgroup25@gmail.com>
2023-07-25 14:02:43 +0300

    daily edu | go | binary search

---
+++ leetcode/852. Peak Index in a Mountain Array/main.go
@@ -0,0 +1,48 @@
+/*
+An array arr a mountain if the following properties hold:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Given a mountain array arr,
+return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+You must solve it in O(log(arr.length)) time complexity.
+
+Example 1:
+    Input: arr = [0,1,0]
+    Output: 1
+
+Example 2:
+    Input: arr = [0,2,1,0]
+    Output: 1
+
+Example 3:
+    Input: arr = [0,10,5,2]
+    Output: 1
+
+Constraints:
+    ** 3 <= arr.length <= 10^5
+    ** 0 <= arr[i] <= 10^6
+    ** arr is guaranteed to be a mountain array.
+*/
+
+package main
+
+func peakIndexInMountainArray(A []int) int {
+	n := len(A)
+	lo, hi := 0, n
+
+	for lo < hi {
+		mid := lo + ((hi - lo) >> 1)
+
+		if A[mid] <= A[mid+1] {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+
+	return lo
+}
